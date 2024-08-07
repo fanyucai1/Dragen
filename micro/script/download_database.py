1c3a12ad9a853dd37eed3c255ba9d089a2f93903
fanyucai1 <fanyucai1@126.com>
2024-08-07 14:30:13 +0800

    no

--- micro/script/download_database.py
+++ micro/script/download_database.py
@@ -11,12 +11,10 @@ os.removedirs(args.datbase)
 os.mkdir(args.database)
 #####################################https://clades.nextstrain.org/dataset
 database_name=['nextstrain/sars-cov-2',
-               'nextstrain/flu/h1n1pdm/ha','nextstrain/flu/h1n1pdm/na','nextstrain/flu/h1n1pdm/pa','nextstrain/flu/h1n1pdm/mp',
-               'nextstrain/flu/h1n1pdm/np','nextstrain/flu/h1n1pdm/ns','nextstrain/flu/h1n1pdm/pb2','nextstrain/flu/h1n1pdm/pb1',
-               'nextstrain/flu/h3n2/ha','nextstrain/flu/h3n2/na','nextstrain/flu/h3n2/pb1','nextstrain/flu/h3n2/np',
-               'nextstrain/flu/h3n2/ns','nextstrain/flu/h3n2/mp','nextstrain/flu/h3n2/pa','nextstrain/flu/h3n2/pb2',
-               'nextstrain/flu/vic/ha','nextstrain/flu/vic/na','nextstrain/flu/yam/ha',
-               'nextstrain/rsv/a','nextstrain/rsv/b',
+               'nextstrain/flu/h1n1pdm/ha/CY121680','nextstrain/flu/h1n1pdm/ha/MW626062','nextstrain/flu/h1n1pdm/na/MW626056','nextstrain/flu/h1n1pdm/pa','nextstrain/flu/h1n1pdm/mp','nextstrain/flu/h1n1pdm/np','nextstrain/flu/h1n1pdm/ns','nextstrain/flu/h1n1pdm/pb2','nextstrain/flu/h1n1pdm/pb1',
+               'nextstrain/flu/h3n2/ha/CY163680','nextstrain/flu/h3n2/ha/EPI1857216','nextstrain/flu/h3n2/na/EPI1857215','nextstrain/flu/h3n2/pb1','nextstrain/flu/h3n2/np','nextstrain/flu/h3n2/ns','nextstrain/flu/h3n2/mp','nextstrain/flu/h3n2/pa','nextstrain/flu/h3n2/pb2',
+               'nextstrain/flu/vic/ha/KX058884','nextstrain/flu/vic/na/CY073894','nextstrain/flu/yam/ha/JN993010',
+               'nextstrain/rsv/a/EPI_ISL_412866','nextstrain/rsv/b/EPI_ISL_1653999',
                'nextstrain/mpox/all-clades',
                'community/neherlab/hiv-1/hxb2',
                'community/moncla-lab/iav-h5/ha/all-clades']
